3f0d56c7d2758ca8f111867c2680ed9b9095abcc
Steve Johns <steve@stevejay.net>
2019-01-22 20:30:09 +0000

    wip

--- src/shared/Toolbar/Toolbar.tsx
+++ src/shared/Toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, SyntheticEvent } from "react";
+import React, { ReactElement } from "react";
 import styles from "./Toolbar.module.scss";
 
 const ARROW_LEFT = 37;
@@ -19,38 +19,43 @@ const Toolbar: React.FunctionComponent<Props> = ({
 }) => {
   const divRef = React.useRef<HTMLDivElement>(null);
   const [tabIndex, setTabIndex] = React.useState(-1);
+  const childRefs = React.useRef<Array<React.RefObject<any>>>([]);
 
-  // React.useLayoutEffect(
-  //   () => {
-  //     if (divRef && divRef.current) {
-  //       const childNodes = divRef.current.children;
-  //       if (tabIndex >= 0 && tabIndex < childNodes.length) {
-  //         (childNodes[tabIndex] as HTMLElement).focus();
-  //       }
-  //     }
-  //   },
-  //   [tabIndex]
-  // );
+  React.useLayoutEffect(
+    () => {
+      if (
+        tabIndex >= 0 &&
+        childRefs &&
+        childRefs.current &&
+        childRefs.current.length > tabIndex
+      ) {
+        const childRef: any = childRefs.current[tabIndex];
+        childRef.focus && childRef.focus();
+      }
+    },
+    [tabIndex]
+  );
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.keyCode === ARROW_LEFT) {
-      setTabIndex(
-        tabIndex <= 0 ? React.Children.count(children) - 1 : tabIndex - 1
-      );
-    } else if (event.keyCode === ARROW_RIGHT) {
-      setTabIndex(
-        tabIndex >= React.Children.count(children) - 1
-          ? 0
-          : tabIndex === -1
-          ? 1
-          : tabIndex + 1
-      );
+    switch (event.keyCode) {
+      case ARROW_LEFT:
+        setTabIndex(
+          tabIndex <= 0 ? React.Children.count(children) - 1 : tabIndex - 1
+        );
+        break;
+      case ARROW_RIGHT:
+        setTabIndex(
+          tabIndex >= React.Children.count(children) - 1
+            ? 0
+            : tabIndex === -1
+            ? 1
+            : tabIndex + 1
+        );
+        break;
+      default:
+        break;
     }
   };
-
-  // const handleClick = () => {
-  //   console.log("got click");
-  // };
 
   return (
     <div
@@ -60,18 +65,13 @@ const Toolbar: React.FunctionComponent<Props> = ({
       className={`${styles.toolbar} ${className}`}
       role="toolbar"
       onKeyDown={handleKeyDown}
-      // onClick={handleClick}
     >
       {React.Children.map(children, (child, index) =>
         React.cloneElement(child as ReactElement<any>, {
           tabIndex:
             tabIndex === -1 && index === 0 ? 0 : index === tabIndex ? 0 : -1,
-          "data-toolbar-index": index,
-          onClick: (event: any) => {
-            setTabIndex(
-              parseInt(event.target.getAttribute("data-toolbar-index"), 10)
-            );
-          }
+          ref: (ref: any) => childRefs.current.push(ref),
+          onClick: () => setTabIndex(index)
         })
       )}
     </div>
--- src/shared/Toolbar/stories/Toolbar.stories.tsx
+++ src/shared/Toolbar/stories/Toolbar.stories.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { host } from "storybook-host";
 // import { withKnobs, boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
+import styles from "./temp.module.scss";
 import Toolbar from "..";
 import "../../../index.scss";
 
@@ -11,13 +12,31 @@ const buttonHost = host({
   backdrop: "transparent"
 });
 
+class Button extends React.Component {
+  _ref: HTMLButtonElement | null = null;
+  focus = () => {
+    this._ref && this._ref.focus();
+  };
+  render() {
+    const { children, ...rest } = this.props;
+    return (
+      <button
+        ref={ref => (this._ref = ref)}
+        {...rest}
+        className={styles.button}
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
 storiesOf("Toolbar", module)
   .addDecorator(buttonHost)
-  // .addDecorator(withKnobs)
   .add("Basic", () => (
     <Toolbar ariaLabel="The toolbar label">
-      <button onClick={noop}>One</button>
-      <button onClick={noop}>Two</button>
-      <button onClick={noop}>Three</button>
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
     </Toolbar>
   ));
